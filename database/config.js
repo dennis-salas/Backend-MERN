d946f35d68f48ec03fafaf25bc3353f45fc6d329
dennis-salas <dgsl93.ds@gmail.com>
2021-10-06 14:28:55 -0500

    base de datos

--- database/config.js
+++ database/config.js
@@ -2,6 +2,10 @@
 'use strict';
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/mern_calendar');
+mongoose.connect('mongodb://localhost:27017/mern_calendar',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+    });
 
 module.exports = mongoose;
