ef9d8c8dad4f35a8e4c32a0408a53662d1b9c9ab
dennis-salas <dgsl93.ds@gmail.com>
2021-10-06 12:35:14 -0500

    readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Backend MERN - Calendar
 
 Backend del curso de React de cero a experto
+
+## Tecnologias Usadas
+
+Node.js
+Express
+Base de Datos MongoDB
+Express-validator
+mongoose
+Json Web Token
+moment
+
+### Es un backend de un login y CRUD para agregar eventos a un calendario
--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use('/api/events', require('./routes/events'));
 
 
 //Escucha peticiones
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Servidor corriendo en el puerto ${process.env.PORT || 5000}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
